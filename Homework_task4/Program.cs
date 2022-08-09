ee41b9345b9770ecb957ff1bfd2766a49c001da6
SvetlanaOgarkova <s-eremeeva@mail.ru>
2022-08-09 17:39:04 +0300

    Task4

---
+++ Homework_task4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите целое число от 1 до N");
+string? numberString = Console.ReadLine();
+int N = int.Parse(numberString!);
+int index = N;
+Console.WriteLine("Вывод четных чисел:");
+
+while(index>1)
+{
+  if(index%2 == 0)
+    {
+      Console.WriteLine (index--);
+    }
+index --;
+}
