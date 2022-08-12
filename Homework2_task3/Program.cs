56574fb24a89a12fdc758408dfa8d1f0228a65d4
SvetlanaOgarkova <s-eremeeva@mail.ru>
2022-08-12 13:31:00 +0300

    HW2task3

---
+++ Homework2_task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным
+
+Console.WriteLine("Введите число от 1 до 7");
+string? numberString = Console.ReadLine();
+int number = int.Parse(numberString!);
+
+if (number >= 6)
+{
+    Console.WriteLine ("Да");
+}
+
+else
+
+Console.WriteLine ("Нет");
