4bb4e13570243c53b3ad27aa5ac7918633017a4f
SvetlanaOgarkova <s-eremeeva@mail.ru>
2022-08-12 13:28:45 +0300

    HW2task1

---
+++ Homework2_task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа
+
+
+Console.WriteLine("Введите трехзначное число");
+string? numberString = Console.ReadLine();
+int number = int.Parse(numberString!);
+int firstnumber = 0;
+int secondnumber = 0;
+
+while (number<0)
+{
+    number = number*(-1);
+}
+if (number>0)
+{
+firstnumber = number /10;
+secondnumber = firstnumber %10;
+Console.WriteLine ("Вторая цифра числа = " + secondnumber);
+}
