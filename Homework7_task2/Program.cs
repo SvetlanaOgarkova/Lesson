8729a1fbf778dd981e180740a18a22a4662a1700
SvetlanaOgarkova <s-eremeeva@mail.ru>
2022-08-31 14:19:13 +0300

    HW7 task2

---
+++ Homework7_task2/Program.cs
@@ -0,0 +1,68 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//17 -> такого числа в массиве нет
+
+void PrintArray(int[,] array)
+{
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] CreateArrayWithRandomNumbers(int m, int n)
+{
+    int[,] result = new int[m, n];
+
+    var random = new Random();
+
+    for (var i = 0; i < result.GetLength(0); i++)
+    {
+        for (var j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = random.Next(1, 100);
+        }
+    }
+
+    return result;
+}
+void ElementPosition(int[,] array, int positionA, int positionB)
+{
+    if (positionA <= array.GetLength(0) && positionB <= array.GetLength(1))
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Элемент на этой позиции = " + array[positionA, positionB] + ".");
+    }
+    else Console.WriteLine("Такого элемента нет.");
+
+}
+
+Console.WriteLine("Введите число строк (m)");
+if (!int.TryParse(Console.ReadLine()!, out var m))
+{
+    Console.WriteLine("Всё плохо");
+}
+Console.WriteLine("Введите число столбцов (n)");
+if (!int.TryParse(Console.ReadLine()!, out var n))
+{
+    Console.WriteLine("Всё плохо");
+}
+Console.WriteLine("Введите первый индекс элемента:");
+int positionA = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите второй индекс элемента:");
+int positionB = int.Parse(Console.ReadLine()!);
+
+int[,] array = CreateArrayWithRandomNumbers(m, n);
+Console.WriteLine();
+PrintArray(array);
+Console.WriteLine();
+ElementPosition(array, positionA, positionB);
