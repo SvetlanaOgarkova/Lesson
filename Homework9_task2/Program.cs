e757bacf9ac02dbf54e63d3e8466681182653d90
SvetlanaOgarkova <s-eremeeva@mail.ru>
2022-09-06 18:48:14 +0300

    HW9 task2

---
+++ Homework9_task2/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNaturalNumber(int m, int n)
+{
+    if (m == n) return n;
+
+    else if (m < n) return n + SumNaturalNumber(m, n - 1);
+
+    else return m + SumNaturalNumber(n, m - 1);
+
+}
+
+int m = 4;
+int n = 8;
+Console.WriteLine(SumNaturalNumber(m, n));
